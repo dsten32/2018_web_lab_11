b3b72abc6c23a93666465d8c9eb93beaa48ff86f
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-12-11 12:44:30 +1300

    done upto ex03

--- sql/ictgradschool/web/lab11/ex2/exercise02.sql
+++ sql/ictgradschool/web/lab11/ex2/exercise02.sql
@@ -1,1 +1,24 @@
 -- Answers to Exercise 2 here
+DROP TABLE IF EXISTS dwc1_programers;
+
+CREATE TABLE IF NOT EXISTS dwc1_programers (
+  username varchar(15) NOT NULL,
+  first_name varchar(32),
+  last_name varchar(32),
+  email varchar(64),
+  PRIMARY KEY (username),
+  CHECK (email LIKE '%@%')
+);
+
+INSERT INTO dwc1_programers VALUES
+                                   ('programmer1','Bill','Gates','bill@microsoft.com'),
+                                   ('programmer2','Peter','Personman','my@email.com'),
+                                   ('programmer3','Pete','Himson','my2@email.com'),
+                                   ('programmer4','Amy','Bill','my@email.com'),
+                                   ('programmer5','Jamie','Look','my@email.com'),
+                                   ('programmer6','Manny','Suck','my@email.com'),
+                                   ('programmer7','Superman','Hi','my@email.com'),
+                                   ('programmer8','Zippy','Dipstick','my@email.com'),
+                                   ('programmer9','Dude','Imaginatron','my@email.com'),
+                                   ('programmer10','MrMan','Sherbert','my@email.com'),
+                                   ('programmer11','Me','Herbert','my@email.com');
--- sql/ictgradschool/web/lab11/ex3/exercise03.sql
+++ sql/ictgradschool/web/lab11/ex3/exercise03.sql
@@ -1,1 +1,40 @@
 -- Answers to Exercise 3 here
+DROP TABLE IF EXISTS dwc1_rentals;
+
+CREATE TABLE IF NOT EXISTS dwc1_rentals (
+  customer_num INT NOT NULL AUTO_INCREMENT,
+  first_name varchar(32),
+  last_name varchar(32),
+  gender varchar(6),
+  dob char(4),
+  join_date YEAR,
+  num_hires INT,
+  PRIMARY KEY (customer_num)
+);
+
+INSERT INTO dwc1_rentals (first_name, last_name, gender, dob, join_date, num_hires) VALUES
+                                                                                           ("Peter","Jackson","male",1961,1997,17000),
+                                                                                           ("Jane","Campion","female",1954,1980,30000),
+                                                                                           ("Roger","Donaldson","male",1945,1980,12000),
+                                                                                           ("Temuera","Morrison","male",1960,1995,15500),
+                                                                                           ("Russell","Crowe","male",1964,1990,10000),
+                                                                                           ("Lucy","Lawless","female",1968,1995,5000),
+                                                                                           ("Michael","Hurst","male",1957,2000,15000),
+                                                                                           ("Andrew","Niccol","male",1964,1997,3500),
+                                                                                           ("Kiri","Te Kanawa","female",1944,1997,500),
+                                                                                           ("Lorde","","female",1996,2010,1000),
+                                                                                           ("Scribe","","male",1979,2000,5000),
+                                                                                           ("Kimbra","","female",1990,2005,7000),
+                                                                                           ("Neil","Finn","male",1958,1985,6000),
+                                                                                           ("Anika","Moa","female",1980,2000,700),
+                                                                                           ("Bic","Runga","female",1976,1995,5000),
+                                                                                           ("Ernest","Rutherford","male",1871,1930,4200),
+                                                                                           ("Kate","Sheppard","female",1847,1930,1000),
+                                                                                           ("Apirana","Ngata","male",1874,1920,3500),
+                                                                                           ("Edmund","Hillary","male",1919,1955,10000),
+                                                                                           ("Katherine","Mansfield","female",1888,1920,2000),
+                                                                                           ("Margaret","Mahy","female",1936,1985,5000),
+                                                                                           ("John","Key","male",1961,1990,20000),
+                                                                                           ("Sonny","Williams","male",1985,1995,15000),
+                                                                                           ("Dan","Carter","male",1982,1990,20000),
+                                                                                           ("Bernice","Mene","female",1975,1990,30000);
